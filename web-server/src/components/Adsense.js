8523f6d7818709bebcbe54fec9fa07f01e726134
Robin Karlberg <karrob@protonmail.ch>
2024-04-10 16:15:14 +0200

    test

--- web-server/src/components/Adsense.js
+++ web-server/src/components/Adsense.js
@@ -26,8 +26,6 @@ export default function Adsense({ data_ad_client, data_ad_slot, className }) {
         }
     }, [])
 
-    return <></>
-
     return (
         <ins className={"adsbygoogle " + className}
             style={{ display: "block", backgroundColor: "white" }}
