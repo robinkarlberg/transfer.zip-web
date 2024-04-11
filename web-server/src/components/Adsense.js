d6400beb58738984765791dbf883030e29064d49
Robin Karlberg <karrob@protonmail.ch>
2024-04-11 23:24:35 +0200

    disable ads by default

--- web-server/src/components/Adsense.js
+++ web-server/src/components/Adsense.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 
 export default function Adsense({ data_ad_client, data_ad_slot, className }) {
-    const ads = true;//process?.env?.REACT_APP_ADSENSE
+    const ads = false;//process?.env?.REACT_APP_ADSENSE
 
     useEffect(() => {
         if(!ads) {
