1e7173e3cc5a68fc80bb46520d7a51442e74c377
Robin Karlberg <karrob@protonmail.ch>
2024-07-02 00:14:56 +0200

    ad placement test

--- web-server/src/App.js
+++ web-server/src/App.js
@@ -12,16 +12,23 @@ import Header from "./components/app/Header";
 
 function App() {
   return (
-    <div className={"App bg-dark-subtle vh-100 d-flex flex-column flex-md-row"}>
-      <Header className="d-md-none" />
-      <SideBar className="d-none d-md-flex" />
-      <div className="d-flex flex-row flex-grow-1 flex-sm-grow-1 overflow-y-scroll">
-        <Outlet />
-      </div>
-      {/* <div className="w-100">
+    <div className="d-flex flex-row">
+      <div className={"App flex-grow-1 bg-dark-subtle vh-100 d-flex flex-column flex-md-row"}>
+        <Header className="d-md-none" />
+        <SideBar className="d-none d-md-flex" />
+        <div className="d-flex flex-row flex-grow-1 flex-sm-grow-1 overflow-y-scroll">
+          <Outlet />
+        </div>
+        {/* <div className="w-100">
         <div className="m-auto" style={{ maxWidth: "1500px" }}>
         </div>
       </div> */}
+      </div>
+      <div className="flex-grow-1 d-none d-sm-inline-block" style={{ maxWidth: "400px" }}>
+        <Adsense data_ad_client="ca-pub-9550547294674683" data_ad_slot="5132630574" />
+        <Adsense data_ad_client="ca-pub-9550547294674683" data_ad_slot="5132630574" />
+        <Adsense data_ad_client="ca-pub-9550547294674683" data_ad_slot="5132630574" />
+      </div>
     </div>
   );
 }
