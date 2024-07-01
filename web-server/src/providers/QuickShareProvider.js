19248cdabfefe857639f4e7e569d9229c30a54e2
Robin Karlberg <karrob@protonmail.ch>
2024-07-02 00:26:07 +0200

    correct link

--- web-server/src/providers/QuickShareProvider.js
+++ web-server/src/providers/QuickShareProvider.js
@@ -32,7 +32,7 @@ export const QuickShareProvider = () => {
         )
         const jwk = await crypto.subtle.exportKey("jwk", key)
 
-        onLink(`${window.location.origin}#${jwk.k},${sessionId},`)
+        onLink(`${window.location.origin}/#${jwk.k},${sessionId},`)
 
         rtcSession.onclose = () => {
             console.log("[QuickShareProvider] [listen] onclose")
