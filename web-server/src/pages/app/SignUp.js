63b8991dabf3bcbeae120580076858f7e6e870b7
Robin Karlberg <karrob@protonmail.ch>
2024-07-28 23:28:24 +0200

    change url of button

--- web-server/src/pages/app/SignUp.js
+++ web-server/src/pages/app/SignUp.js
@@ -28,7 +28,7 @@ export default function SignUp({ }) {
         try {
             const res = await Api.register(emailFieldRef.current.value, passwordFieldRef.current.value)
             if (res.success) {
-                navigate(getParams.get("success") || "/about/pricing", {
+                navigate(getParams.get("success") || "/pricing", {
                     state: {
                         newSignUp: true
                     }
