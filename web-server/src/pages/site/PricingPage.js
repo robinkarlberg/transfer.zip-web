5d8e29a8aad604339a0ce4baa6e657cfe6c841d9
Robin Karlberg <karrob@protonmail.ch>
2024-07-28 23:26:00 +0200

    change button

--- web-server/src/pages/site/PricingPage.js
+++ web-server/src/pages/site/PricingPage.js
@@ -102,7 +102,7 @@ export default function PricingPage({ }) {
                                 <li>Share by QR code or link</li>
                                 <li>1GB storage</li>
                             </ul>
-                            <a type="button" href="/" className="w-100 btn btn-lg btn-outline-primary">Open app</a>
+                            <a type="button" href="/app" className="w-100 btn btn-lg btn-outline-primary">Open app</a>
                         </div>
                     </div>
                 </div>
