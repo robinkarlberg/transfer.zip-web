b36a64847f4a3d7507cafe13fb679b8ff2ce1dbf
Filipsuper <filipkarlberg1@gmail.com>
2024-08-01 20:05:18 +0200

    update API_URL assignment to use nullish coalescing operator

--- web-server/src/api/Api.js
+++ web-server/src/api/Api.js
@@ -2,7 +2,7 @@ import { isSelfHosted } from "../utils"
 import streamSaver from "../lib/StreamSaver"
 streamSaver.mitm = "/mitm.html"
 
-export const API_URL = !process.env.REACT_APP_API_URL ? ("http://localhost:8001") : (process.env.REACT_APP_API_URL);
+export const API_URL = process.env.REACT_APP_API_URL ?? "https://localhost:8001"
 
 const get = async (endpoint, extraHeaders) => {
     if (isSelfHosted()) throw new Error("Tried to make an API call, but is Self Hosting", endpoint)
