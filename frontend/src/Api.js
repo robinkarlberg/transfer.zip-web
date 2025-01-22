59efa73c944c757482957764257898b807845c83
Robin Karlberg <karrob@protonmail.ch>
2025-01-22 16:38:46 +0100

    add new upload functionality

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -113,3 +113,95 @@ export async function getStripeAccount(tenantId) {
 export async function joinWaitlist(email) {
     return await post(`/waitlist/join`, { email })
 }
+
+// transfer
+
+const CHUNK_SIZE = 10 * 1024 * 1024 // 10MB
+
+export function uploadTransferFiles(transferId, files, onProgress) {
+    return new Promise((resolve, reject) => {
+        // Initialize the WebSocket connection
+        const apiUrlWithoutProtocol = API_URL.replace(/^https?:/, '')
+        const wsProtocol = window.location.protocol == "https:" ? "wss:" : "ws:"
+        const ws = new WebSocket(`${wsProtocol}//${apiUrlWithoutProtocol}/upload/${transferId}`)
+
+        const startFileTransfer = (fileIndex) => {
+            const currentFile = files[fileIndex]
+            ws.send(JSON.stringify({
+                fileInfo: { relativePath: currentFile.webkitRelativePath || currentFile.name, name: currentFile.name, size: currentFile.size, type: currentFile.type }
+            }))
+
+            const sendChunk = (start) => {
+                const nextChunk = currentFile.slice(start, start + CHUNK_SIZE)
+                const reader = new FileReader()
+
+                reader.onload = async (e) => {
+                    // TODO: wait if neccessary
+
+                    ws.send(e.target.result)
+                    onProgress && onProgress({
+                        fileIndex,
+                        progress: Math.min(start + CHUNK_SIZE, currentFile.size) / currentFile.size
+                    })
+
+                    if (start + CHUNK_SIZE < currentFile.size) {
+                        sendChunk(start + CHUNK_SIZE)
+                    } else {
+                        fileIndex++
+                        if (fileIndex < files.length) {
+                            startFileTransfer(fileIndex + 1)
+                        }
+                        else {
+                            ws.send(JSON.stringify({
+                                finished: true
+                            }))
+                        }
+                    }
+                }
+                reader.readAsArrayBuffer(nextChunk)
+            }
+
+            fileIndex++
+            sendChunk(0)
+        }
+
+        ws.onopen = () => {
+            startFileTransfer(0)
+        }
+
+        ws.onmessage = (event) => {
+            // Handle incoming WebSocket messages (progress updates)
+            console.log(event.data)
+            try {
+                const message = event.data
+                const jsonObject = JSON.parse(message)
+
+                // Handle completion (final result)
+                if (jsonObject.finished) {
+                    ws.close()
+                    resolve()
+                }
+                else if (!jsonObject.success) {
+                    throw new Error(jsonObject.message)
+                }
+            } catch (error) {
+                reject(error)
+                ws.close()
+            }
+        }
+
+        ws.onerror = (error) => {
+            console.error("WebSocket error:", error)
+            reject(error)
+        }
+
+        ws.onclose = () => {
+            console.log("WebSocket connection closed")
+        }
+    })
+}
+
+
+export async function newTransfer() {
+    return await post(`/transfer/new`, {})
+}
