7ff5d9a1ef6fe0a0590837b65eb6eab41bcff9ae
Robin Karlberg <karrob@protonmail.ch>
2025-04-01 23:15:23 +0200

    new settings page design

--- frontend/src/routes/dashboard/SettingsPage.js
+++ frontend/src/routes/dashboard/SettingsPage.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import GenericPage from "../../components/dashboard/GenericPage";
 import { AuthContext } from "../../providers/AuthProvider";
 import ManageSubscriptionButton from "../../components/ManageSubscriptionButton";
@@ -17,7 +17,7 @@ const timeZoneOptions = moment.tz.names();
 export default function SettingsPage({ }) {
   const { user, isFreeUser } = useContext(AuthContext)
 
-  const { setShowUpgradeModal } = useContext(DashboardContext)
+  const { setShowUpgradeModal, hideSidebar } = useContext(DashboardContext)
 
   const planValidUntil = new Date(user.planValidUntil)
   const options = {
@@ -32,6 +32,10 @@ export default function SettingsPage({ }) {
     window.location.href = "/"
   }
 
+  useEffect(() => {
+    hideSidebar()
+  }, [])
+
   const { tiers } = pricing
 
   const [selectedMailingLists, setSelectedMailingLists] = useState(tiers[0])
@@ -43,44 +47,41 @@ export default function SettingsPage({ }) {
   return (
     <GenericPage title={"Settings"}>
       <div className="pt-4">
-        <div className="border-b border-gray-900/10 pb-12 max-w-xl">
-          <h2 className="text-base/7 font-semibold text-gray-900 ">Account</h2>
+        <div className="border rounded-2xl shadow-sm p-6 border-gray-900/10 max-w-xl mb-8">
+          <h2 className="text-lg font-semibold text-gray-900 ">Account</h2>
           <p className="mt-1 text-sm/6 text-gray-600">To change your email or delete your account, <a className="text-primary" href={`mailto:${process.env.REACT_APP_SUPPORT_EMAIL}`}>contact us</a>.</p>
 
           <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
             <div className="sm:col-span-4">
-              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+              <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
                 Email address
               </label>
               <div className="mt-2">
-                <input
-                  defaultValue={user.email}
-                  id="email"
-                  name="email"
-                  type="email"
+                <span
                   disabled
                   autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm/6"
-                />
+                  className="text-gray-900 sm:text-sm/6"
+                >
+                  {user.email}
+                </span>
               </div>
             </div>
           </div>
         </div>
-        <div className="mt-8 border-gray-900/10 pb-12 max-w-xl">
-          <h2 className="text-base/7 font-semibold text-gray-900 ">Subscription</h2>
+        <div className="border rounded-2xl shadow-sm p-6 border-gray-900/10 max-w-xl mb-8">
+          <h2 className="text-lg font-semibold text-gray-900 ">Subscription</h2>
           <p className="mt-1 text-sm/6 text-gray-600">View and change your subscription details.</p>
 
           <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
             <div className="sm:col-span-4">
               <div className="mb-2">
-                <span className="font-bold text-gray-800 me-2">Current Plan:</span>
-                <div className="inline-block text-primary font-semibold bg-primary-subtle rounded-lg px-2.5 py-1.5">{capitalizeFirstLetter(user.plan)}</div>
+                <span className="font-bold text-gray-800 me-2">{capitalizeFirstLetter(user.plan)}</span>
               </div>
               {user.plan == "starter" && <button type="button" onClick={handleUpgrade} className={"text-primary rounded-md font-medium text-sm hover:text-primary-light mb-2"}>Upgrade Subscription &rarr;</button>}
               <form method="POST" action={API_URL + "/create-customer-portal-session"}>
-                <button type="submit" className={"text-primary rounded-md font-medium text-sm hover:text-primary-light"}>Manage Billing</button>
+                <button type="submit" className={"text-primary rounded-md font-medium text-sm hover:text-primary-light"}>Manage Billing &rarr;</button>
               </form>
             </div>
           </div>
