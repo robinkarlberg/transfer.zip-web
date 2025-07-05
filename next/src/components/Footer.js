e6de57e11e8cd5eca65fac62d95bf58cf4169ca6
Robin Karlberg <karrob@protonmail.ch>
2025-07-05 17:42:42 +0200

    add bsky url

--- next/src/components/Footer.js
+++ next/src/components/Footer.js
@@ -94,6 +94,10 @@ export default function Footer({ }) {
               </svg>
               <span className="sr-only">Discord community</span>
             </a>}
+            {process.env.NEXT_PUBLIC_BLUESKY_URL && <a href={process.env.NEXT_PUBLIC_BLUESKY_URL} className="text-gray-500 hover:text-gray-900 --dark:hover:text-white ms-5">
+              <BIcon center name={"bluesky"} />
+              <span className="sr-only">Bluesky page</span>
+            </a>}
             {process.env.NEXT_PUBLIC_TWITTER_URL && <a href={process.env.NEXT_PUBLIC_TWITTER_URL} className="text-gray-500 hover:text-gray-900 --dark:hover:text-white ms-5">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
                 <path fillRule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clipRule="evenodd" />
