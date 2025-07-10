82f336f9e1d28886938a666389103800f61b5541
Robin Karlberg <karrob@protonmail.ch>
2025-07-10 22:06:50 +0200

    dont modify cookies in useServerAuth

--- next/src/lib/server/wrappers/auth.js
+++ next/src/lib/server/wrappers/auth.js
@@ -15,7 +15,7 @@ export async function useServerAuth() {
   const session = await Session.findOne({ token }).populate("user").exec();
 
   if (!session || !session.user) {
-    cookieStore.delete("token")
+    // cookieStore.delete("token")
     throw new Error("Invalid session");
   }
 
