1e1ffc7ff543e447befaf25827ae0b5dbc65c8a9
Robin Karlberg <karrob@protonmail.ch>
2025-07-23 15:17:04 +0200

    fix getAllSlugs in content.js in docker build

--- next/src/lib/server/content.js
+++ next/src/lib/server/content.js
@@ -1,8 +1,8 @@
 import path from 'path'
 import { globby } from 'globby'
-const contentRoot = path.join(process.cwd(), 'content')
 
 export async function getAllSlugs() {
-  const files = await globby(['**/*.mdx'], { cwd: contentRoot })
+  // Different paths in docker build (process.env.NODE_ENV == "production") and in local development
+  const files = await globby(['**/*.mdx'], { cwd: path.join(process.cwd(), process.env.NODE_ENV == "production" ? 'src/content' : 'content') })
   return files.map(f => f.replace(/\.mdx$/, '')) // e.g. guides/getting-started
 }
