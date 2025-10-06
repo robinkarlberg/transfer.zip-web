a1763b4febd677dc608bc74115f7db43444b11e5
Robin Karlberg <karrob@protonmail.ch>
2025-10-06 19:37:06 +0200

    edit message

--- next/src/app/(app)/app/DismissibleBanner.js
+++ next/src/app/(app)/app/DismissibleBanner.js
@@ -27,7 +27,7 @@ export default function DismissibleBanner() {
             <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
           </svg>
           <p className="text-white text-sm md:text-base">
-            <span className="font-semibold">Note:</span> Sending files by email is temporarily unavailable. Your transfers will continue to work normally.
+            <span className="font-semibold">Note:</span> We are investigating an issue where emails are not being delivered. Your transfers will continue to work normally.
           </p>
         </div>
         <button
