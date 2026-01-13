59ef9a693459ed516e3f8d0a8283f0da144110e9
Robin Karlberg <karrob@protonmail.ch>
2026-01-13 18:23:23 +0100

    fix bug in upload

--- next/src/app/(site)/(downloadupload)/upload/[secretCode]/page.js
+++ next/src/app/(site)/(downloadupload)/upload/[secretCode]/page.js
@@ -10,6 +10,7 @@ import TestimonialCloud from "@/components/TestimonialCloud";
 import FAQ from "@/components/FAQ";
 import Image from "next/image";
 import NewTransferFileUploadForRequest from "@/components/newtransfer/NewTransferFileUploadForRequest";
+import IndieStatement from "@/components/IndieStatement";
 
 export default async function ({ params }) {
   const { secretCode } = await params
@@ -36,12 +37,21 @@ export default async function ({ params }) {
             src={brandProfile.backgroundUrl}
           />
         )}
-        <NewTransferFileUploadForRequest brandProfile={brandProfile.friendlyObj()} transferRequest={transferRequest.friendlyObj()} />
+        <NewTransferFileUploadForRequest brandProfile={brandProfile?.friendlyObj()} transferRequest={transferRequest.friendlyObj()} />
       </div>
       {(!IS_SELFHOST && !brandProfile) && (
         <>
           <Features1 />
           <TestimonialCloud />
+          <div className="relative">
+            <div className="w-full h-screen overflow-hidden absolute grain bg-linear-to-b from-primary-600 to-primary-300" />
+            <div className="py-24 px-2 sm:px-8 relative">
+              <p className="text-center mt-2 text-pretty text-3xl font-bold tracking-tight text-white sm:text-3xl lg:text-balance text-shadow-md">
+                A quick message from the founder.
+              </p>
+              <IndieStatement compact />
+            </div>
+          </div>
           <FAQ />
         </>
       )}
